def035c870e5a9f36f4ade8dc748c465412b6f62
TyraeN <112875201+TyraeN1337@users.noreply.github.com>
2025-10-02 19:37:07 +0100

    Delete src directory

--- src/pages/Admin.jsx
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <section>
-      <h2>Panel administracyjny</h2>
-      <p>Tajna zawartość (lazy loaded).</p>
-    </section>
-  );
-}
--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <section>
-      <h1>Witaj w sklepie</h1>
-      <p>Demo z Data Router: loadery, akcje, lazy, ochrona i 404.</p>
-    </section>
-  );
-}
